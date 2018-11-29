a727cba894ab1784b513484b2f16aa8b4762157e
Unknown <kemal.kautsar@gmail.com>
2018-11-29 19:25:00 +0700

    add: installing NPM and kurento
    to be continued...

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-/*
-*  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
-*
-*  Use of this source code is governed by a BSD-style license
-*  that can be found in the LICENSE file in the root of the source
-*  tree.
-*/
-'use strict';
+import * as streamedian from 'streamedian/player.js';
+// import WebsocketTransport from 'wsp/transport/websocket';
+// import RTSPClient from 'wsp/client/rtsp/client';
 
-const leftVideo = document.getElementById('leftVideo');
-const rightVideo = document.getElementById('rightVideo');
-
-leftVideo.addEventListener('canplay', () => {
-  const stream = leftVideo.captureStream();
-  rightVideo.srcObject = stream;
+let mediaElement = rtsp.attach(document.getElementById('test_video'));
+let player = new streamedian.WSPlayer(mediaElement, {
+    // url: `${STREAM_URL}`,      // overrides mediaElement's sources
+    modules: [
+        {
+            // client module constructor. Should be subclass or BaseClient. RTSPClient by default
+            // client: RTSPClient,
+            transport: {
+               // client module constructor. Should be subclass or BaseTransport. WebsocketTransport by default
+               // constructor: WebsocketTransport,
+               options: {
+                   // address of websocket proxy described below. ws${location.protocol=='https:'?'s':''}://${location.host}/ws/ by default
+                   socket: "ws://websocket_proxy_address/ws"
+               }
+           }
+        },
+    ]
 });
